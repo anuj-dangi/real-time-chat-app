92e8c2578399bc85a8155f61db4e6650f137e029
vsaa-org <23mcce16@uohyd.ac.in>
2025-06-22 23:38:12 +0530

    codebase for login page

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import React from 'react';
+import { useState } from "react";
+import { useAuthStore } from '../store/useAuthStore.js';
 
 const LoginPage = () => {
+
+  const [ FormData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const [ showPassword, setShowPassword] = useState(false);
+
+  const { login, isLoggingIn } = useAuthStore();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    login(FormData);
+  };
+
   return (
     <div>
-      LoginPage
+      
     </div>
   )
 }
 
-export default LoginPage
+export default LoginPage;
