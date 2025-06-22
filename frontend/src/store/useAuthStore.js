8575ebe41b352a5ef5856d64d072d0050b3a3758
vsaa-org <23mcce16@uohyd.ac.in>
2025-06-22 18:57:48 +0530

    Completed signup page

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -1,5 +1,6 @@
 import { create } from "zustand";
 import { axiosInstance } from "../lib/axios.js";
+import { toast } from "react-hot-toast";
 
 export const useAuthStore = create((set) => ({
     authUser: null,
@@ -28,6 +29,35 @@ export const useAuthStore = create((set) => ({
     },
 
     signup: async (data) => {
-        
+        set({ isSigningUp: true });
+
+        try
+        {
+            const res = await axiosInstance.post("/auth/signup", data);
+            set({ authUser : res.data});
+            toast.success("Account Created Successfully");
+        }
+        catch(error)
+        {
+            toast.error(error.response.data.message);
+        }
+        finally
+        {
+            set({ isSigningUp: false });
+        }
+    },
+
+    logout: async() => {
+        try
+        {
+            const res = await axiosInstance.post("/auth/logout");
++6          set({ authUser: null });
+            toast.success("Logged out successfully");
+
+        }
+        catch(error)
+        {
+            toast.error(error.response.data.message);
+        }
     }
 }));
