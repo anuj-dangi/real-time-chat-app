77fed99ee26f0d0d708fc660247876b99dba9fd3
vsaa-org <23mcce16@uohyd.ac.in>
2025-06-23 09:01:38 +0530

    codebase for login page

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { useState } from "react";
 import { useAuthStore } from '../store/useAuthStore.js';
+import { Mail, MessageSquare, Lock, Eye, EyeOff, Loader2 } from "lucide-react";
+import { Link } from "react-router-dom";
 
 const LoginPage = () => {
 
-  const [ FormData, setFormData] = useState({
+  const [ formData, setFormData] = useState({
     email: "",
     password: "",
   });
@@ -19,10 +21,97 @@ const LoginPage = () => {
   };
 
   return (
-    <div>
-      
-    </div>
-  )
+    <div className="min-h-screen grid lg:grid-cols-1">
+          
+          {/* left side */}
+          <div className="flex flex-col justify-center items-center p-6 sm:p-12">
+            <div className="w-full max-w-md space-y-8">
+              {/* LOGO */}
+              <div className="text-center mb-8">
+                <div className="flex flex-col items-center gap-2 group">
+                  <div
+                    className="size-12 rounded-xl bg-primary/10 flex items-center justify-center 
+                  group-hover:bg-primary/20 transition-colors"
+                  >
+                    <MessageSquare className="size-6 text-primary" />
+                  </div>
+                  <h1 className="text-2xl font-bold mt-2">Welcome Back</h1>
+                  <p className="text-base-content/60">Sign in to your account</p>
+                </div>
+              </div>
+
+              <form onSubmit={handleSubmit} className="space-y-6">
+
+                <div className="form-control">
+                  <label className="label">
+                    <span className="label-text font-medium">Email</span>
+                  </label>
+                  <div className="relative">
+                    <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none z-10 ">
+                    <Mail className="size-5 text-base-content/40" />
+                    </div>
+                    <input
+                      type="email"
+                      className={`input input-bordered w-full pl-10`}
+                      placeholder="You@example.com"
+                      value={formData.email}
+                      onChange={(e) => setFormData({ ...formData, email: e.target.value })}
+                    />
+                  </div>
+                </div>
+
+                <div className="form-contorl">
+                  <label className="label">
+                    <span className="label-text font-medium">Password</span>
+                  </label>
+                  <div className="relative">
+                    <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none z-10 ">
+                    <Lock className="size-5 text-base-content/40" />
+                    </div>
+                    <input
+                      type={showPassword ? "text" : "password"}
+                      className={'input input-bordered w-full pl-10'}
+                      placeholder="••••••••"
+                      value={formData.password}
+                      onChange={(e) => setFormData({ ...formData, password: e.target.value})}
+                    />
+                    <button 
+                      type="button" 
+                      className="absolute inset-y-0 right-0 pr-3 flex items-center z-10" 
+                      onClick={() => setShowPassword(!showPassword)}>
+                        {showPassword ? (<EyeOff className="size-5 text-base-content/40"/>) :  (<Eye className="size-5 text-base-content/40"/>)}
+                    </button>
+                  </div>
+                </div> 
+
+                <button
+                  type="submit"
+                  className="btn btn-primary w-full"
+                  disabled={isLoggingIn}
+                >
+                  {isLoggingIn ? (
+                    <>
+                      <Loader2 className="size-5 animate-spin"/>
+                      Loading...
+                    </>
+                  ) : (
+                    "Sign in"
+                  )}
+                </button>
+              </form>
+
+              <div className="text-center">
+                <p className="text-base-content/60">
+                  Don&apos;t have an account?{" "}
+                  <Link to="/signup" className="link link-primary">
+                    Create Account
+                  </Link>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      )
 }
 
 export default LoginPage;
