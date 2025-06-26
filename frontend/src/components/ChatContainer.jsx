04615dc2804bd0596a11efa598c7b8e0b8a5fdff
Anuj Dangi <23mcce16@uohyd.ac.in>
2025-06-26 22:09:54 +0530

    initial chat container

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { useEffect} from 'react'
+import { useChatStore } from '../store/useChatStore.js';
 
 const ChatContainer = () => {
+
+  const { messages, getMessages, isMessageLoading, selectedUser } = useChatStore();
+
+  useEffect(() => {
+    gerMessage(selectedUser._id)
+  }, [selectedUser._id, getMessages]);
+
+  if(isMessageLoading)  return <div>Loading...</div>
+
   return (
-    <div>
-      ChatContainer
+    <div className='flex-1 flex flex-col overflow-auto'>
+      <ChatHeader />
+
+      <p>messages...</p>
+
+      <MessageInput />
+      
     </div>
   )
 };
