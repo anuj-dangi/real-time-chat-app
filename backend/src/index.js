0de78b7331011a20fe0aadac062acef47378f305
Anuj Dangi <23mcce16@uohyd.ac.in>
2025-06-27 13:47:45 +0530

    updating index.js for socket util

--- backend/src/index.js
+++ backend/src/index.js
@@ -10,13 +10,15 @@ import { connectDB } from "./lib/db.js";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 
+import { app, server } from "./lib/socket.js";
+
 //config dotenv
 dotenv.config();
-const app = express();
 
 const PORT = process.env.PORT;
 
-app.use(express.json());
+app.use(express.json({limit: '10mb'}));
+app.use(express.urlencoded({extended: true, limit: '10mb'}))
 app.use(cookieParser());
 app.use(cors({
     origin: "http://localhost:5173",
@@ -28,7 +30,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes)
 
 //listening on port 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log("Server is running on PORT : " + PORT);
     connectDB();
 });
